a1bf097b86f73de4981b50ab5ec6268c78326cfb
rubberhead <bjchenzhengyi@hotmail.com>
2023-01-06 20:07:01 +0000

    What a drag... D8 done. Also not feeling well

--- src/get_solutions/day8.rs
+++ src/get_solutions/day8.rs
@@ -1,8 +1,22 @@
 use super::AOCSolutions; 
-use std::{iter::{Iterator, DoubleEndedIterator, Zip}, str::Lines}; 
+use std::{iter::{Iterator, DoubleEndedIterator, Zip}, str::Lines, os::linux::raw}; 
 use core::ops::Index; 
 
 pub struct Day8;
+
+impl AOCSolutions for Day8 {
+    fn get_star_1(input: &str) -> Result<i64, ()> {
+        Ok(Day8::parse(input))
+    }
+
+    fn get_star_2(input: &str) -> Result<i64, ()> {
+        let res: Result<i64, _> = Day8::parse2(input).try_into(); 
+        match res {
+            Ok(v) => Ok(v), 
+            Err(_) => Err(()), 
+        }
+    }
+}
 
 impl Day8 {
     /** 
@@ -12,7 +26,8 @@ impl Day8 {
     This function **consumes** `iterator`. 
 
     Obviously this is a dumb solution to the given problem -- we should be able to find all visible 
-    elements in a view in one single iteration. This is more of a proof-of-concept, I guess?
+    elements in a view in two iterations (one from front, one from back) -- O(2n) time instead of O(n^2) 
+    time for each of the n elements in view. This is more of a proof-of-concept, I guess?
      */
     fn is_visible_in_view<I>(idx: usize, iterator: I) -> bool
         where I: Iterator, 
@@ -43,6 +58,98 @@ impl Day8 {
 
         return true; 
     }
+
+    fn scenic_score_of_idx_along_axis<T: PartialOrd>(idx: usize, axis: &[T]) -> usize {
+        if let Some(ref_v) = axis.get(idx) {
+            let (prev_slice, next_slice) = axis.split_at(idx); 
+            let prev_itr = prev_slice.iter().rev();
+            let prev_result = Day8::find_view_distance(ref_v, prev_itr); 
+
+            let next_itr = next_slice[1..].iter();
+            let next_result = Day8::find_view_distance(ref_v, next_itr); 
+            return prev_result * next_result; 
+        } else {
+            panic!("[Day8::scenic_score_of_idx_along_axis] Out of bounds: `slice` has length {} but {} given as `idx`", axis.len(), idx); 
+        }
+    }
+
+    fn find_eqincreasing_subsequence<T: PartialOrd>(ref_v: T, mut itr: impl Iterator<Item = T>) -> Vec<T> {
+        let mut result: Vec<T> = Vec::new(); 
+        while let Some(next) = itr.next() {
+            match result.last() {
+                None => result.push(next), // first tree encountered 
+                Some(last) => {
+                    if next > *last { // taller tree
+                        result.push(next) 
+                    } else if next == *last && *last < ref_v { // equally-sized tree that is shorter than viewpoint
+                        result.push(next)
+                    }
+                }, 
+            }
+        }
+        return result;
+    }
+
+    fn find_view_distance<T: PartialOrd>(ref_v: T, mut itr: impl Iterator<Item = T>) -> usize {
+        let mut result: usize = 0;
+        while let Some(next) = itr.next() {
+            result += 1;
+            if next >= ref_v { break; }
+        }
+        return result; 
+    }
+
+    fn parse2(input: &str) -> usize {
+        let mut results: Vec<Vec<usize>> = Vec::new();  
+        let row_itr = input.lines().map(str::as_bytes);
+        let col_itr = ColumnIterator::from(input.lines()); 
+
+        row_itr.for_each(|r| {
+            let row_view_scenic_scores: Vec<usize> = r.iter().enumerate()
+                .map(|(c_idx, _)| Day8::scenic_score_of_idx_along_axis(c_idx, r) )
+                .collect(); 
+            results.push(row_view_scenic_scores); 
+        }); 
+
+        let mut max_scenic_score: usize = 0;
+        col_itr.enumerate().for_each(|(c_idx, c)| {
+            c.iter().enumerate().for_each(|(r_idx, _)| {
+                results[r_idx][c_idx] *= Day8::scenic_score_of_idx_along_axis(r_idx, &c); 
+                if results[r_idx][c_idx] > max_scenic_score {
+                    max_scenic_score = results[r_idx][c_idx]; 
+                }
+            })
+        });
+        println!("Done finding max scenic score."); 
+        return max_scenic_score; 
+    }
+
+    fn parse(input: &str) -> i64 {
+        let mut result_mat: Vec<Vec<bool>> = Vec::new(); 
+        let row_itr = input.lines().map(|s| s.as_bytes() ); 
+        let col_itr = ColumnIterator::from(input.lines()); 
+
+        row_itr.for_each(|r| {
+            let row_view_vis_result: Vec<bool> = r.iter().enumerate() 
+                .map(|(c_idx, _)| Day8::is_visible_in_view(c_idx, r.iter())) 
+                .collect(); 
+            result_mat.push(row_view_vis_result); 
+        });  
+        
+        col_itr.enumerate().for_each(|(c_idx, c)| {
+            // println!("{}", c_idx); 
+            c.iter().enumerate().for_each(|(r_idx, _)| {
+                let col_vis_r_c = Day8::is_visible_in_view(r_idx, c.iter()); 
+                result_mat[r_idx][c_idx] |= col_vis_r_c; 
+            })
+        }); 
+        
+        return result_mat.iter()
+            .map(|r| {
+                r.iter().fold(0 as i64, |acc, r| if *r { acc + 1 } else { acc })
+            })
+            .sum();  
+    }
 }
 
 /**
@@ -69,22 +176,36 @@ impl<'a> Iterator for ColumnIterator<'a> {
         if self.is_consumed { return None; }
 
         let mut buf = Vec::new();
-        let mut flag = false; 
+        let mut consumed_flag = true; 
         self.line_itrs.iter_mut()
             .for_each(|i| {
-                if let Some(bc) = i.next() { 
-                    buf.push(bc); 
-                    flag = true; 
+                if let Some(bc) = i.next() {
+                    match bc {
+                        10 => (),  // \n
+                        _ => {
+                            buf.push(bc); 
+                            consumed_flag = false;
+                        },  
+                    }
                 }
             }); 
-        self.is_consumed = flag; 
+        self.is_consumed = consumed_flag; 
+        if self.is_consumed { return None; }
         return Some(buf); 
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Day8; 
+    use crate::get_solutions::AOCSolutions;
+
+    use super::{Day8, ColumnIterator}; 
+
+    const SAMPLE_INPUT: &str = r"30373
+25512
+65332
+33549
+35390";
 
     #[test]
     fn test_is_visible_in_view_1() {
@@ -100,6 +221,20 @@ mod tests {
     }
 
     #[test]
+    fn test_scenic_score(){
+        let axis = "33549";
+        let axis2 = "35353";
+        assert_eq!(Day8::scenic_score_of_idx_along_axis(2, axis.as_bytes()), 4); 
+        assert_eq!(Day8::scenic_score_of_idx_along_axis(3, axis2.as_bytes()), 2); 
+    }
+
+    #[test]
+    fn test_scenic_score_2(){
+        let result = Day8::parse2(SAMPLE_INPUT);
+        println!("Done! {}", result); 
+    }
+
+    #[test]
     fn test_is_visible_in_view_2(){
         let axis_2 = "abccba"; // visible idx: all
         let answer_2 = vec![true, true, true, true, true, true]; 
@@ -110,5 +245,21 @@ mod tests {
             })
             .collect(); 
         assert_eq!(answer_2, result_2); 
+    }
+
+    #[test]
+    fn test_col_iter() {
+        let test_str = "abc\ndef\nghi"; 
+        let mut col_iter = ColumnIterator::from(test_str.lines()); 
+        let sub_u8s = col_iter.next().unwrap(); 
+        // assert!(col_iter.next().is_some()); 
+        assert!(col_iter.next().is_some());
+        assert!(col_iter.next().is_some());
+        assert!(col_iter.next().is_none()); 
+    }
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(Day8::parse(SAMPLE_INPUT), 21); 
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,18 @@ use advent_of_rust_22::get_solutions::AOCSolutions;
 use advent_of_rust_22::get_solutions::*; 
 use day6::Day6;
 use day7::Day7; 
+use day8::Day8; 
 
 // TODO: Write a proper CLI. 
 fn main() {
+    /* 
     let arguments: Vec<String> = env::args().collect(); 
     assert!(arguments.len() == 2); 
     
-    let input = advent_of_rust_22::parse_to_string(& arguments[1]).unwrap(); 
+    let input = advent_of_rust_22::parse_to_string(&arguments[1]).unwrap(); 
+    */
 
-    println!("{}", Day7::get_star_1(input.as_str()).unwrap());
-    println!("{}", Day7::get_star_2(input.as_str()).unwrap()); 
+    let input = advent_of_rust_22::parse_to_string("./inputs/day8/input").unwrap(); 
+    println!("{}", Day8::get_star_1(input.as_str()).unwrap());
+    println!("{}", Day8::get_star_2(input.as_str()).unwrap()); 
 }
